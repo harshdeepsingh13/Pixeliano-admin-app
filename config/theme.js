21fcbafbb51230ac2b1e004a4155420041bce710
Harshdeep Singh <harshdeep.singh@tothenew.com>
2020-02-03 12:54:56 +0530

    added text and borders colors in theme config

--- config/theme.js
+++ config/theme.js
@@ -4,4 +4,14 @@ export default {
     primaryDark: '#16a085',
   },
   dark: {},
+  backgroundAndBorders: {
+    greyDark: '#dbdbdb',
+    greyMedium: '#ebebeb',
+    greyLight: '#f3f3f3',
+  },
+  text: {
+    greyDark: '#565555',
+    greyMedium: '#6c6e6e',
+    greyLight: '#999999',
+  },
 };
