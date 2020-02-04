e11372d851774b08fe9b2451e63bf1d7bf377e5c
Harshdeep Singh <harshdeep.singh@tothenew.com>
2020-02-04 10:23:08 +0530

    added config file

---
+++ config/config.js
@@ -0,0 +1,11 @@
+export default {
+  errorMessages: {
+    requiredFieldEmpty: 'required field',
+    emailNotFormatted: 'enter proper email id',
+    verifyPassword: 'passwords entered do not match',
+    shortPassword: 'Password should be atleast 8 characters long, should be alpha-numeric and should have atleast one special character',
+    websiteNotValid: 'website is not valid',
+    contactNumberNotValid: 'Contact number entered is not valid',
+    numberOutOfRange: 'the number entered is out of given range',
+  },
+};
