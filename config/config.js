bbedb414849716ea720e5a673eb35f733b6d6088
Harshdeep Singh <harshdeep.singh@tothenew.com>
2020-02-06 11:49:56 +0530

    added config variables for 'errorMessages.emailAlreadyExists' and API status'

--- config/config.js
+++ config/config.js
@@ -2,6 +2,7 @@ export default {
   errorMessages: {
     requiredFieldEmpty: 'required field',
     emailNotFormatted: 'enter proper email id',
+    emailAlreadyExists: 'this email already exists.',
     verifyPassword: 'passwords entered do not match',
     shortPassword: 'Password should be atleast 8 characters long, should be alpha-numeric and should have atleast one special character',
     websiteNotValid: 'website is not valid',
@@ -10,5 +11,11 @@ export default {
   },
   emailRegex: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
   passwordRegex: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
-  apiUrl: 'http://localhost:8080/api/v1/'
+  apiUrl: 'http://10.0.2.2:8080/api/v1/',
+  status: {
+    started: 'started',
+    success: 'success',
+    failed: 'failed',
+    default: 'default',
+  },
 };
