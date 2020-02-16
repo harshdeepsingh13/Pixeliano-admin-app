70d72d21b37a3f5a3b427d5ee5fe424540076ede
Harshdeep Singh <harshdeep.singh@tothenew.com>
2020-02-16 15:43:30 +0530

    added button primary colors and added lightRed color in the background colors.

--- config/theme.js
+++ config/theme.js
@@ -2,12 +2,17 @@ export default {
   light: {
     primary: '#1abc9c',
     primaryDark: '#16a085',
+    button: {
+      primary: '#34495e',
+      primaryDark: '#2c3e50',
+    },
   },
   dark: {},
   backgroundAndBorders: {
     greyDark: '#dbdbdb',
     greyMedium: '#ebebeb',
     greyLight: '#f3f3f3',
+    lightRed: "rgba(255,0,0,0.28)"
   },
   text: {
     greyDark: '#565555',
@@ -15,13 +20,14 @@ export default {
     greyLight: '#999999',
   },
   basicColors: {
-    lightBlueGrey: "#d7f3f3",
-    greyishBrown: "#424242",
-    salmon: "#ff6d6d",
-    dusk: "#4a5977",
-    blueyGrey: "#99a1b2",
-    greyishBrownTwo: "#414141",
-    black: "#000000",
-    successGreen: '#4BB543'
-  }
+    lightBlueGrey: '#d7f3f3',
+    greyishBrown: '#424242',
+    salmon: '#ff6d6d',
+    dusk: '#4a5977',
+    blueyGrey: '#99a1b2',
+    greyishBrownTwo: '#414141',
+    black: '#000000',
+    successGreen: '#4BB543',
+    primaryReddish: "#fc5a5c"
+  },
 };
