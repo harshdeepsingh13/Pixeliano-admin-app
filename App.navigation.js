4ed0c52ce2efe218d7e265b73b0f27b2d38774ea
Harshdeep Singh <harshdeep.singh@tothenew.com>
2020-02-16 15:39:05 +0530

    added Signup, register and dashboard navigation routes, with fundamental changes in default navigation options.

--- App.navigation.js
+++ App.navigation.js
@@ -3,6 +3,9 @@ import {createAppContainer} from 'react-navigation';
 import theme from './config/theme';
 import React from 'react';
 import AppIcon from './components/AppIcon';
+import SignIn from './containerComponents/SignIn';
+import Dashboard from './containerComponents/Dashboard';
+import RegisterUser from './containerComponents/RegisterUser';
 
 export default containedComponent => {
   const appNavigator = createStackNavigator(
@@ -13,6 +16,9 @@ export default containedComponent => {
             title: "RSS"
          }*/
       },
+      SignIn: SignIn,
+      RegisterUser:RegisterUser,
+      Dashboard: Dashboard,
     },
     {
       initialRouteName: 'Home',
@@ -25,9 +31,6 @@ export default containedComponent => {
         },
         title: 'RSS Generator',
         headerTintColor: 'white',
-        headerLeft: () => <AppIcon
-          size={42}
-        />,
       },
     },
   );
