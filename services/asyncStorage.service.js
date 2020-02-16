3330e302a38668dc8e15b4de8e3594fa1a9ad743
Harshdeep Singh <harshdeep.singh@tothenew.com>
2020-02-16 15:39:36 +0530

    added Async storage services functions.

---
+++ services/asyncStorage.service.js
@@ -0,0 +1,17 @@
+import AsyncStorage from '@react-native-community/async-storage';
+import config from '../config/config';
+
+export const getToken = async () => {
+  const item = await AsyncStorage.getItem(config.storageKey.userDetails);
+  return (item && JSON.parse(item).token) ? JSON.parse(item).token : undefined;
+};
+
+export const saveItem = async (storageKey = config.storageKey.userDetails, payload = {}) => {
+  await AsyncStorage.setItem(storageKey, JSON.stringify(payload));
+  return 'done';
+};
+
+export const deleteItem = async (storageKey = config.storageKey.userDetails) => {
+  await AsyncStorage.setItem(storageKey, JSON.stringify({}));
+  return 'done';
+};
