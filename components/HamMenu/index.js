5aae71a0d701677d7e7d85e6fdd47f5aefb4319e
Harshdeep Singh <harshdeep.singh@tothenew.com>
2020-02-16 15:44:21 +0530

    Hamburger menu component

---
+++ components/HamMenu/index.js
@@ -0,0 +1,61 @@
+import React, {useRef} from 'react';
+import {View} from 'react-native';
+import style from './styles.js';
+import PropTypes from 'prop-types';
+import Menu from 'react-native-material-menu';
+import HamburgerButton from '../BasicUIElements/HamburgerButton';
+import theme from '../../config/theme';
+import MenuOption from '../MenuOption';
+
+const HamMenu = ({
+                   buttonColor,
+                   menus,
+                 }) => {
+
+  const menuRef = useRef(undefined);
+
+  const showMenu = () => {
+    menuRef.current.show();
+  };
+  const hideMenu = () => {
+    menuRef.current.hide();
+  };
+
+  return (
+    <View>
+      <Menu
+        button={<HamburgerButton color={buttonColor} handleOnClick={showMenu}/>}
+        ref={menuRef}
+      >
+        {
+          menus.map(({
+                       text,
+                       handleClick,
+                     }) => (
+            <MenuOption
+              menuHandleClick={() => {
+                hideMenu();
+                handleClick();
+              }}
+              menuText={text}
+            />
+          ))
+        }
+      </Menu>
+    </View>
+  );
+};
+
+HamMenu.propTypes = {
+  buttonColor: PropTypes.string,
+  menus: PropTypes.array,
+};
+
+HamMenu.defaultProps = {
+  buttonColor: theme.light.primaryDark,
+  menus: [],
+};
+
+HamMenu.navigationOptions = {};
+
+export default HamMenu;
