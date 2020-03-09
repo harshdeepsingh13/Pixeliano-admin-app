f9889ec36d7a3c31e7b2e5deb51cb129084f9655
Harshdeep Singh <harshdeep.singh@tothenew.com>
2020-03-09 20:35:00 +0530

    added getCloudinaryURL function to cloudinary service.

--- services/cloudinary.service.js
+++ services/cloudinary.service.js
@@ -2,6 +2,12 @@ import axios from 'axios';
 import config from '../config/config';
 import qs from 'qs';
 
+const transformationsMapping = {
+  width: 'w',
+  height: 'h',
+  quality: 'q',
+};
+
 export const uploadImage = async (
   imageData,
 ) => {
@@ -16,9 +22,30 @@ export const uploadImage = async (
       transformRequest: (data, headers) =>
         qs.stringify(data),
     });
-    return data
+    return data;
   } catch (e) {
     console.log('eee', e.response.data);
     throw e;
+  }
+};
+
+export const getCloudinaryImageUrl = ({
+                                        publicId,
+                                        transformations,
+                                      }) => {
+  /*
+  transformations ----
+  width
+  height
+  quality
+  * */
+  if (transformations && Object.keys(transformations).length !== 0) {
+    const transformationArray = [];
+    for (let [key, value] of Object.entries(transformations)) {
+      transformationArray.push(`${transformationsMapping[key]}_${value}`);
+    }
+    return `${config.cloudinary.secureDeliveryURL}${transformationArray.join(',')}/${publicId}`;
+  } else {
+    return `${config.cloudinary.secureDeliveryURL}${publicId}`;
   }
 };
