813608e847f48f8a19e3634ea65c73dd19610837
Harshdeep Singh <harshdeep.singh@tothenew.com>
2020-03-18 22:53:55 +0530

    added static assets.

---
+++ config/assets.js
@@ -0,0 +1,12 @@
+export default {
+  logo: {
+    icon: {
+      color: require("../assets/rssGen-iconColor.png"),
+      grey: require("../assets/rssGen-iconGrey.png")
+    },
+    full: {
+      color: require("../assets/rssGen-fullColor.png"),
+      grey: require("../assets/rssGen-fullGrey.png")
+    }
+  }
+}
