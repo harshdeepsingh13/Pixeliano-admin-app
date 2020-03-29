b38bf1c65fbc80130df9035e8ec41c8c23c8ca18
Harshdeep Singh <harshdeep.singh@tothenew.com>
2020-03-29 13:08:33 +0530

    added delete post menu option and delete post loader and error handling.

--- containerComponents/ViewPost/index.js
+++ containerComponents/ViewPost/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {ScrollView, StatusBar, View} from 'react-native';
 import HeaderWithImage from '../../components/HeaderWithImage';
 import theme from '../../config/theme';
@@ -6,11 +6,20 @@ import ViewCaption from '../../components/ViewCaption';
 import TagsContainer from '../../components/TagsContainer';
 import Tag from '../../components/Tag';
 import FloatingButton from '../../components/BasicUIElements/FloatingButton';
+import config from '../../config/config';
+import FullScreenLoader from '../../components/BasicUIElements/FullScreenLoader';
+import {deletePost as deletePostService} from '../../services/axios.service';
+import Error from '../../components/Error';
 
 const ViewPost = ({
                     navigation,
                     route,
                   }) => {
+
+  const [deletePostStatus, setDeletePostStatus] = useState({
+    status: config.status.default,
+    message: '',
+  });
 
   const {
     caption,
@@ -29,8 +38,45 @@ const ViewPost = ({
     [],
   );
 
+  const deletePost = async () => {
+    try {
+      setDeletePostStatus({...deletePostStatus, status: config.status.started});
+      await deletePostService(postId);
+      setDeletePostStatus({...deletePostStatus, status: config.status.success});
+      navigation.goBack();
+    } catch (e) {
+      if (e.isAxiosError && e.response) {
+        setDeletePostStatus({
+          ...deletePostStatus,
+          status: config.status.failed,
+          message: config.errorMessages.APIresponseMessages.login[e.response.data.status],
+        });
+        console.log('ee', e.response.data);
+      } else {
+        setDeletePostStatus({
+          ...deletePostStatus,
+          status: config.status.failed,
+          message: config.errorMessages.APIresponseMessages[500],
+        });
+      }
+    }
+  };
+
   return (
     <View style={{height: '100%'}}>
+      {
+        deletePostStatus.status === config.status.started &&
+        <FullScreenLoader
+          loadingMessage={'Deleting Post'}
+        />
+      }
+      {
+        deletePostStatus.status === config.status.failed &&
+        <Error
+          errorType={'displayError'}
+          message={deletePostStatus.message}
+        />
+      }
       <ScrollView
         scrollEventThrottle={16}
         stickyHeaderIndices={[0]}
@@ -38,6 +84,25 @@ const ViewPost = ({
         <HeaderWithImage
           imageProvider={picture.providerName}
           imageShortName={picture.shortName}
+          isOptionsMenu={true}
+          menus={
+            [
+              {
+                text: 'Delete Post',
+                handleClick: () => {
+                  console.log('deletePost');
+                  deletePost();
+                },
+              },
+              {
+                text: 'Upload on Instagram',
+                handleClick: () => {
+                  console.log('uploadOn Instagram');
+                },
+                disabled: true,
+              },
+            ]
+          }
         />
         <ViewCaption
           caption={caption}
